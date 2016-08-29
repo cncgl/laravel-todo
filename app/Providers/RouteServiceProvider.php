98dc68284c18e7884ff21bd7b5f248a7836c0558
cncgl <concigel@gmail.com>
2016-08-29 23:52:26 +0900

    migrate to laravel 5.3

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,11 +22,13 @@ class RouteServiceProvider extends ServiceProvider
      * @param  \Illuminate\Routing\Router  $router
      * @return void
      */
-    public function boot(Router $router)
+    // public function boot(Router $router)
+    public function boot()
     {
         //
 
-        parent::boot($router);
+        // parent::boot($router);
+        parent::boot();
     }
 
     /**
